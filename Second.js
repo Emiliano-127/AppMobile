fd46effcbd79d55c9c0103f1e63a0559511b81e8
Emiliano-127 <@emilianoSaraiva2019>
2023-07-29 14:57:44 -0300

    atual

---
+++ Second.js
@@ -0,0 +1,65 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function Second(){
+
+    return(
+        <View style={styles.container}>
+
+<TextInput 
+                style ={{height: 30, width: '80%', borderColor: 'red', borderBottomWidth: 3, margin:50}}
+                placeholder="Nome" 
+                
+            />
+
+            <TextInput 
+                style ={{height: 30, width: '80%', borderColor: 'red', borderBottomWidth: 3, margin:50}}
+                placeholder="Email" 
+                
+            />
+
+            <TextInput 
+                style ={{height: 30, width: '80%', borderColor: 'red', borderBottomWidth: 3, margin: 50}}
+                placeholder="Senha" 
+                
+            />
+
+            <Button title={'Sign-Up'}/>
+
+        </View>
+    );
+};
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+
+        flex: 0.7,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        margin: 20,
+        height: 5,
+        backgroundColor: '#fff',
+        borderRadius: 30,
+        borderColor: 'gray',
+        borderWidth: 1,
+        
+        
+
+    },
+
+    lines:{
+        width: 200,
+        height: 5,
+        color: "blue",
+    },
+
+    text:{
+        margin: 30,
+        color: "red",
+        
+    }
+});
